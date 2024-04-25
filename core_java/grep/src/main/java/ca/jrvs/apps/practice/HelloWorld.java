7fcb295744beabbc10c9e963d8ca7ceab2293847
Ayodele Ajimoko <ayodeleajimoko@gmail.com>
2024-04-25 17:42:29 +0000

    setting up maven for grep app

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -0,0 +1,16 @@
+package main.java.ca.jrvs.apps.practice;
+
+
+public class HelloWorld{
+  // Program will start with a call to main
+  //
+
+    public static void main(String[] args) {
+      System.out.println("Hello World!");
+    }
+}
+
+
+
+
+
