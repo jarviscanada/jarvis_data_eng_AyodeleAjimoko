1509937897f3bde57eaaa2d82c07395385ca2377
Ayodele Ajimoko <ayodeleajimoko@gmail.com>
2024-05-15 21:01:28 +0000

    driver not found error

---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -0,0 +1,24 @@
+package main.java.ca.jrvs.apps.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+
+public class JDBCExecutor {
+  public static void main(String... args){
+    DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplusport", "postgres", "password");
+    try{
+      Connection connection = dcm.getConnection();
+      Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
+      while(resultSet.next()){
+        System.out.println(resultSet.getInt(1));
+      }
+    }catch (SQLException e){
+      e.printStackTrace();
+    }
+  }
+}
